ed5826e5b144126ef98ced1c3446262e40c12c0a
plorenzom <plorenzo.manzano@gmail.com>
2024-09-08 20:56:29 +0200

    Initial commit

---
+++ src/main/java/es/thefactory/hellospringdatajpa/Application.java
@@ -0,0 +1,19 @@
+package es.thefactory.hellospringdatajpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Pablo Lorenzo Manzano.
+ */
+@SpringBootApplication
+public class Application {
+    
+    /**
+     *
+     * @param args
+     */
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
