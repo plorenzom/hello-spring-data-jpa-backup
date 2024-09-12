04fe8bd4e8f69e0a32a75bcae36a499a54f8a060
plorenzom <plorenzo.manzano@gmail.com>
2024-09-12 16:55:32 +0200

    Add demo CRUD operations with a JPA repository

--- src/main/java/es/thefactory/hellospringdatajpa/Application.java
+++ src/main/java/es/thefactory/hellospringdatajpa/Application.java
@@ -1,13 +1,31 @@
 package es.thefactory.hellospringdatajpa;
 
+import es.thefactory.hellospringdatajpa.dal.entity.Carmaker;
+import es.thefactory.hellospringdatajpa.dal.repo.CarmakerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.Optional;
+import java.util.logging.Logger;
 
 /**
  * @author Pablo Lorenzo Manzano.
  */
 @SpringBootApplication
-public class Application {
+public class Application implements CommandLineRunner {
+    
+    /**
+     *
+     */
+    private static final Logger LOGGER = Logger.getLogger(Application.class.getName());
+    
+    /**
+     *
+     */
+    @Autowired
+    private CarmakerRepository carmakerRepo;
     
     /**
      *
@@ -15,5 +33,51 @@ public class Application {
      */
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+    }
+    
+    /**
+     *
+     * @param args
+     * @throws Exception
+     */
+    @Override
+    public void run(String... args) throws Exception {
+        LOGGER.info("Entering method run()");
+        final short VOLKSWAGEN_GROUP_ID = 1;
+        
+        LOGGER.info("Añadir fabricantes de coches:");
+        carmakerRepo.save(new Carmaker("Volkswagen Group", "-"));
+        carmakerRepo.save(new Carmaker("Toyota Motor", "Japón"));
+        carmakerRepo.save(new Carmaker("Mercedes-Benz Group", "Alemania"));
+        carmakerRepo.save(new Carmaker("Ford Motor", "EEUU"));
+        carmakerRepo.findAllByOrderByCarmakerId().forEach(carmaker -> {
+            LOGGER.info(carmaker.getName() + "/" + carmaker.getOriginCountry());
+        });
+        LOGGER.info("");
+        
+        LOGGER.info("Actualizar el país de origen del fabricante 'Volkswagen Group':");
+        Optional<Carmaker> optionalCarmaker = carmakerRepo.findById(VOLKSWAGEN_GROUP_ID);
+        optionalCarmaker.ifPresent(carmaker -> {
+            carmaker.setOriginCountry("Alemania");
+            carmakerRepo.save(carmaker);
+        });
+        carmakerRepo.findAllByOrderByCarmakerId().forEach(carmaker -> {
+            LOGGER.info(carmaker.getName() + "/" + carmaker.getOriginCountry());
+        });
+        LOGGER.info("");
+        
+        LOGGER.info("Listar fabricantes de 'Alemania':");
+        carmakerRepo.findByOriginCountry("Alemania").forEach(carmaker -> {
+            LOGGER.info(carmaker.getName() + "/" + carmaker.getOriginCountry());
+        });
+        LOGGER.info("");
+        
+        LOGGER.info("Eliminar el fabricante 'Volkswagen Group':");
+        carmakerRepo.deleteById(VOLKSWAGEN_GROUP_ID);
+        carmakerRepo.findAll().forEach(carmaker -> {
+            LOGGER.info(carmaker.getName() + "/" + carmaker.getOriginCountry());
+        });
+        
+        LOGGER.info("Exiting method run()");
     }
 }
